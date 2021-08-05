b624d145e881e27d9e3bdeaa68baeff839e55c0d
Gohar Ashraf <gohar.ashraf78611@gmail.com>
2021-08-05 12:23:10 +0500

    first commit

---
+++ models/campground.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const CampgroundSchema = mongoose.Schema({
+    title: String,
+    price: String,
+    description: String,
+    location: String
+});
+
+
+module.exports = mongoose.model('Campground',CampgroundSchema);
