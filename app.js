93ddee538c8e52fb9bb709efd8f23a458bf46bc5
Gohar Ashraf <gohar.ashraf78611@gmail.com>
2021-08-07 09:26:40 +0500

    7th Aug v-1

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const morgan = require("morgan");
+const { campgroundSchema } = require("./schemas");
+const ExpressError = require("./utlis/ExpressError");
+const catchAsync = require("./utlis/catchAsync");
 const Campground = require("./models/campground");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
@@ -27,57 +30,91 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(morgan("dev"));
 
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+};
+
 app.get("/", (req, res) => {
     res.render("home");
 });
 
-app.get("/campgrounds", async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render("campgrounds/index", { campgrounds });
-});
+app.get(
+    "/campgrounds",
+    catchAsync(async (req, res) => {
+        const campgrounds = await Campground.find({});
+        res.render("campgrounds/index", { campgrounds });
+    })
+);
 
 app.get("/campgrounds/create", (req, res) => {
     res.render("campgrounds/create");
 });
 
-app.post("/campgrounds", async (req, res, next) => {
-    try {
+app.post(
+    "/campgrounds",
+    validateCampground,
+    catchAsync(async (req, res, next) => {
         const campground = new Campground(req.body.campground);
         await campground.save();
         res.redirect(`/campgrounds/${campground._id}`);
-    } catch (error) {
-        next(error);
-    }
-});
+    })
+);
 
-app.get("/campgrounds/:id", async (req, res) => {
-    let id = req.params.id;
-    const campground = await Campground.findById(id);
-    res.render("campgrounds/show", { campground });
-});
+app.get(
+    "/campgrounds/:id",
+    catchAsync(async (req, res) => {
+        let id = req.params.id;
+        const campground = await Campground.findById(id);
+        res.render("campgrounds/show", { campground });
+    })
+);
 
-app.get("/campgrounds/:id/edit", async (req, res) => {
-    const id = req.params.id;
-    const campground = await Campground.findById(id);
-    res.render("campgrounds/edit", { campground });
-});
+app.get(
+    "/campgrounds/:id/edit",
+    catchAsync(async (req, res) => {
+        const id = req.params.id;
+        const campground = await Campground.findById(id);
+        res.render("campgrounds/edit", { campground });
+    })
+);
 
-app.put("/campgrounds/:id", async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {
-        ...req.body.campground,
-    });
-    res.redirect(`/campgrounds/${campground._id}`);
-});
+app.put(
+    "/campgrounds/:id",
+    validateCampground,
+    catchAsync(async (req, res) => {
+        const { id } = req.params;
+        const campground = await Campground.findByIdAndUpdate(id, {
+            ...req.body.campground,
+        });
+        res.redirect(`/campgrounds/${campground._id}`);
+    })
+);
 
-app.delete("/campgrounds/:id", async (req, res) => {
-    let id = req.params.id;
-    await Campground.findByIdAndDelete(id);
-    res.redirect("/campgrounds");
+app.delete(
+    "/campgrounds/:id",
+    catchAsync(async (req, res) => {
+        let id = req.params.id;
+        await Campground.findByIdAndDelete(id);
+        res.redirect("/campgrounds");
+    })
+);
+
+app.all("*", (req, res, next) => {
+    next(new ExpressError("Page Not Found", 404));
 });
 
 app.use((err, req, res, next) => {
-    res.send("lol");
+    const { status = 500, message = "Something went wrong" } = err;
+    if (!err.message) {
+        err.message = "Something went wrong";
+    }
+    res.status(status).render("error", { err });
 });
 
 app.listen(3000, () => {
